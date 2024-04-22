2767dc028db8b1adcd01b47a9743297b6239974f
REIGaruki <gyakujo@mail.ru>
2024-04-22 13:04:50 +0300

    Encapsulate

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,7 @@
 public class Car extends Transport{
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
